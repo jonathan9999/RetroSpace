4bac0c44bd3fcd729c8f37a5a8a85b470864229f
Nir Boneh <nirboneh@Manojs-MacBook-Pro.local>
2014-06-29 12:50:31 -0600

    loading screen

---
+++ RetroSpace/LoadingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadingViewController.h
+//  RetroSpace
+//
+//  Created by Nir Boneh on 6/29/14.
+//  Copyright (c) 2014 Clouby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingViewController : UIViewController
+
+@end
---
+++ RetroSpace/LoadingViewController.m
@@ -0,0 +1,31 @@
+//
+//  LoadingViewController.m
+//  RetroSpace
+//
+//  Created by Nir Boneh on 6/29/14.
+//  Copyright (c) 2014 Clouby. All rights reserved.
+//
+
+#import "LoadingViewController.h"
+
+@interface LoadingViewController ()
+
+@end
+
+@implementation LoadingViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [NSThread sleepForTimeInterval:2.0f];
+    [self performSegueWithIdentifier:@"loadToMain" sender:self];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
