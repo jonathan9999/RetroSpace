8d1c9dc516934ffff0dc3d711e4944a02f296b62
Nir Boneh <nirboneh@Manojs-MacBook-Pro.local>
2014-06-29 11:39:46 -0600

    Initial Commit

---
+++ RetroSpace/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RetroSpace
+//
+
+//  Copyright (c) 2014 Clouby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RetroSpace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RetroSpace
+//
+//  Created by Nir Boneh on 6/29/14.
+//  Copyright (c) 2014 Clouby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
